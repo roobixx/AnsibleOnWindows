84c98b9a0f195dd988c54d66c48b197ddd752e19
Tim Fowler <tim@roobixx.com>
2021-10-19 14:24:21 -0500

    Create ansible-playbook.cmd

---
+++ ansible-playbook.cmd
@@ -0,0 +1,15 @@
+@echo off
+REM "alias" for packer ansible provisioner running in cmd/powershell
+set args=%*
+REM replace backslashes with slashes
+call set args=%%args:\=/%%
+REM fix drive letters
+call set args=%%args:C:=/mnt/c%%
+REM call set args=%%args:D:=/mnt/d%%
+REM call set args=%%args:E:=/mnt/e%%
+call set args=%%args:/"=%%
+call set args=%%args:"'='%%
+call set args=%%args:'"='%%
+call set args=%%args:"=^'%%
+
+C:\Windows\System32\bash.exe -c "ansible-playbook %args%"
